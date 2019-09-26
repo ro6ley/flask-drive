2bda6b1d31014dc2336d00a799171325ad771586
Robley <robleyadrian@gmail.com>
2019-09-26 12:45:42 +0300

    add main Flask app file

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+
+from flask import Flask, render_template, request, redirect, send_file, url_for
+
+from s3_demo import list_files, download_file, upload_file
+
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+BUCKET = "flaskdrive"
+
+
+@app.route('/')
+def entry_point():
+    return 'Hello World!'
+
+
+@app.route("/storage")
+def storage():
+    contents = list_files("flaskdrive")
+    return render_template('storage.html', contents=contents)
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(f.filename)
+        upload_file(f"{f.filename}", BUCKET)
+
+        return redirect("/storage")
+
+
+@app.route("/download/<filename>", methods=['GET'])
+def download(filename):
+    if request.method == 'GET':
+        output = download_file(filename, BUCKET)
+
+        return send_file(output, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
