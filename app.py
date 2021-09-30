30bbb2c59be353669812a8c837fd7962a7c5aef1
Denis Trofimov <denis.trofimov@huma.com>
2021-09-30 18:44:48 +0300

    ref: create a temporary directory using the context manager
    Signed-off-by: Denis Trofimov <denis.trofimov@huma.com>

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
+import tempfile
 
 from flask import Flask, render_template, request, redirect
 from s3_demo import list_files, upload_file
 from werkzeug.utils import secure_filename
 
 
-app = Flask(__name__)
 BUCKET=os.getenv('BUCKET')
 
+app = Flask(__name__)
 
 @app.route('/')
 def storage():
@@ -19,9 +20,12 @@ def storage():
 def upload():
     if request.method == "POST":
         f = request.files['file']
-        f.save(secure_filename(f.filename))
-        upload_file(f"{secure_filename(f.filename)}", BUCKET)
-
+        # create a temporary directory using the context manager
+        with tempfile.TemporaryDirectory() as dir:
+            print('created temporary directory', dir)
+            path = os.path.join(dir, secure_filename(f.filename))
+            f.save(path)
+            upload_file(path, BUCKET, secure_filename(f.filename))
         return redirect("/")
 
 
--- s3_demo.py
+++ s3_demo.py
@@ -1,11 +1,12 @@
 import boto3
 
 
-def upload_file(file_name, bucket):
+def upload_file(file_name, bucket, object_name=""):
     """
     Function to upload a file to an S3 bucket
     """
-    object_name = file_name
+    if not object_name:
+        object_name = file_name
     s3_client = boto3.client('s3')
     response = s3_client.upload_file(file_name, bucket, object_name)
 
